29e8edcd6edfe9a72c62933d926f5cd1908230c2
arzzzzi <kantyshevmagomed@gmail.com>
2022-12-15 15:37:01 +0300

    Created products component and rendered them

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,15 @@
+import { data } from '../utils/data';
 import Layout from '../components/Layout';
+import PrdouctItem from '../components/PrdouctItem';
 
 export default function Home() {
-  return <Layout title="Home Page">Home page</Layout>;
+  return (
+    <Layout title="Home Page">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <PrdouctItem product={product} key={product.slug} />
+        ))}
+      </div>
+    </Layout>
+  );
 }
---
+++ utils/data.js
@@ -0,0 +1,63 @@
+export const data = {
+    products: [{
+            name: 'Free shirt',
+            slug: 'free-shirt',
+            category: 'Shirts',
+            image: '/images/shirt1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'A popular shirt'
+        },
+        {
+            name: 'Fit shirt',
+            slug: 'fit-shirt',
+            category: 'Shirts',
+            image: '/images/shirt2.jpg',
+            price: 80,
+            brand: 'Adidas',
+            rating: 3.2,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A popular shirt'
+        },
+        {
+            name: 'Slim shirt',
+            slug: 'slim-shirt',
+            category: 'Shirts',
+            image: '/images/shirt3.jpg',
+            price: 90,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 3,
+            countInStock: 20,
+            description: 'A popular shirt'
+        },
+        {
+            name: 'Sweatpants',
+            slug: 'sweatpants',
+            category: 'Pants',
+            image: '/images/pants1.jpg',
+            price: 90,
+            brand: 'New Balance',
+            rating: 2.9,
+            numReviews: 13,
+            countInStock: 20,
+            description: 'Comforable sweatpants'
+        },
+        {
+            name: 'Fit shirt',
+            slug: 'fit-shirt',
+            category: 'Pants',
+            image: '/images/pants2.jpg',
+            price: 95,
+            brand: 'Zara',
+            rating: 3.5,
+            numReviews: 7,
+            countInStock: 20,
+            description: 'A popular pants'
+        }
+    ]
+}

1d1aa13dbf399ece075f0831fe63c26d32ad2248
arzzzzi <kantyshevmagomed@gmail.com>
2022-12-21 14:46:53 +0300

    added user menu

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,20 +1,29 @@
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
 import Link from 'next/link';
 import { useContext, useEffect, useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import { Store } from '../utils/Store';
 import 'react-toastify/dist/ReactToastify.css';
+import { Menu } from '@headlessui/react';
+import DropdownLink from './DropdownLink';
+import Cookies from 'js-cookie';
 
 const Layout = ({ title, children }) => {
   const { status, data: session } = useSession();
 
-  const { state } = useContext(Store);
+  const { state, dispatch } = useContext(Store);
   const { cart } = state;
   const [cartitemsCount, setCartitemsCount] = useState(0);
   useEffect(() => {
     setCartitemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
   }, []);
+
+  const logoutHandler = () => {
+    Cookies.remove('cart');
+    dispatch({ type: 'CART_RESET' });
+    signOut({ callbackUrl: '/login' });
+  };
   return (
     <>
       <Head>
@@ -43,7 +52,26 @@ const Layout = ({ title, children }) => {
               {status === 'loading' ? (
                 'Loading'
               ) : session?.user ? (
-                session.user.name
+                <Menu as="div" className="relative inline-block">
+                  <Menu.Button className="text-blue-600">{session.user.name}</Menu.Button>
+                  <Menu.Items className="absolute right-0 w-56 origin-top-right shadow-lg bg-white">
+                    <Menu.Item>
+                      <DropdownLink className="dropdown-link" href={'/profile'}>
+                        Profile
+                      </DropdownLink>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <DropdownLink className="dropdown-link" href={'/order-history'}>
+                        Order History
+                      </DropdownLink>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <a className="dropdown-link" href="#" onClick={logoutHandler}>
+                        Logout
+                      </a>
+                    </Menu.Item>
+                  </Menu.Items>
+                </Menu>
               ) : (
                 <Link legacyBehavior href={'/login'}>
                   <a className="p-2">Login</a>
--- utils/Store.js
+++ utils/Store.js
@@ -27,6 +27,15 @@ function reducer(state, action) {
       Cookies.set('cart', JSON.stringify({ ...state.cart, cartItems }));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
+    case 'CART_RESET':
+      return {
+        ...state,
+        cart: {
+          cartItems: [],
+          shippingAddress: { location: {} },
+          paymentMethod: '',
+        },
+      };
     default:
       return state;
   }
@@ -35,5 +44,5 @@ function reducer(state, action) {
 export function StoreProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
   const value = { state, dispatch };
-  return <Store.Provider value={value}>{children}</Store.Provider>;
+  return <Store.Provider value={value}> {children} </Store.Provider>;
 }

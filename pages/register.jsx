5183cf2b8868b5627c9f44a805f4ed9589f49052
arzzzzi <kantyshevmagomed@gmail.com>
2022-12-26 17:12:06 +0300

    Created registration page

--- pages/register.jsx
+++ pages/register.jsx
@@ -6,6 +6,7 @@ import { getError } from '../utils/error';
 import { signIn, useSession } from 'next-auth/react';
 import { toast } from 'react-toastify';
 import { useRouter } from 'next/router';
+import axios from 'axios';
 
 export default function LoginPage() {
   const { data: session } = useSession();
@@ -22,12 +23,17 @@ export default function LoginPage() {
   const {
     register,
     handleSubmit,
-    watch,
+    getValues,
     formState: { errors },
   } = useForm();
 
-  const submitHandler = async ({ email, password }) => {
+  const submitHandler = async ({ name, email, password }) => {
     try {
+      await axios.post('/api/auth/signup', {
+        name,
+        email,
+        password,
+      });
       const result = await signIn('credentials', {
         redirect: false,
         email,
@@ -42,9 +48,22 @@ export default function LoginPage() {
   };
 
   return (
-    <Layout title={'Login'}>
+    <Layout title={'Create Account'}>
       <form className="mx-auto max-w-screen-md" onSubmit={handleSubmit(submitHandler)}>
-        <h1 className="mb-4 text-xl">Login</h1>
+        <h1 className="mb-4 text-xl">Create Account</h1>
+        <div className="mb-4">
+          <label htmlFor="name">Name</label>
+          <input
+            {...register('name', {
+              required: 'Please enter name',
+            })}
+            type="text"
+            className="w-full"
+            id="name"
+            autoFocus
+          />
+          {errors.name && <div className="text-red-500">{errors.name.message}</div>}
+        </div>
         <div className="mb-4">
           <label htmlFor="email">Email</label>
           <input
@@ -58,7 +77,6 @@ export default function LoginPage() {
             type="email"
             className="w-full"
             id="email"
-            autoFocus
           />
           {errors.email && <div className="text-red-500">{errors.email.message}</div>}
         </div>
@@ -72,17 +90,35 @@ export default function LoginPage() {
             type="password"
             className="w-full"
             id="password"
-            autoFocus
           />
           {errors.password && <div className="text-red-500">{errors.password.message}</div>}
         </div>
         <div className="mb-4">
-          <button className="primary-button">Login</button>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            {...register('confirmPassword', {
+              required: 'Please confirm your password',
+              validate: (value) => value === getValues('password'),
+              minLength: { value: 6, message: 'Password contains 6 or more chars' },
+            })}
+            type="password"
+            className="w-full"
+            id="confirmPassword"
+          />
+          {errors.confirmPassword && (
+            <div className="text-red-500">{errors.confirmPassword.message}</div>
+          )}
+          {errors.confirmPassword && errors.confirmPassword.type === 'validate' && (
+            <div className="text-red-500">Passwords do not match</div>
+          )}
         </div>
         <div className="mb-4">
-          Don&apos;t have an accout? &nbsp;
-          <Link legacyBehavior href={`/register?redirect=${redirect || '/'}`}>
-            <a>Sign up</a>
+          <button className="primary-button">Create Account</button>
+        </div>
+        <div className="mb-4">
+          Already have an accout? &nbsp;
+          <Link legacyBehavior href={`/login?redirect=${redirect || '/'}`}>
+            <a>Sign In</a>
           </Link>
         </div>
       </form>
